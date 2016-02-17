d9ee109b5fbeefe0caeb1fd4ebd1e15e9aad9416
ana gomez <gomezselva2011@gmail.com>
2016-02-17 00:42:51 -0600

    back steps

--- main.py
+++ main.py
@@ -1,43 +1,26 @@
-import urllib2
-from urllib2 import urlopen
+from urllib.request import urlopen
 import re
-import cookielib
-from cookielib import CookieJar
 import time
 #https://www.youtube.com/watch?v=CMyoQbBq5IE
 #http://www.elnuevodiario.com.ni/rss/
 #http://www.tn8.tv/comments/feed/
-cj = CookieJar()
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+
 
 def main():
     
-    page = 'http://www.laprensa.com.ni/feed'
-    page1 = 'http://www.elnuevodiario.com.ni/rss/'
+    page = 'http://www.elnuevodiario.com.ni/rss/sucesos'
     crawl(page)
-    crawl(page1)
     
     
 def crawl(page):
     
-    try:
-        sourceCode = opener.open(page).read()
-        print ('----------------------------------------')
-        titles= re.findall(r'<title>(.*?)</title>', sourceCode)
-        for title in titles:
-            print title
-            
-        
-                
-    except Exception, e:
-        print str(e)
+    sourceCode = urlopen(page).read()
     
-    
-    
-    
-    
-    
+    print ('----------------------------------------')
+    titles= re.findall(b'<title>(.*?)</title>', sourceCode)	
+	#asi es?te dormiste?
+    for title in titles:
+        print (title)    
 
 if __name__ == "__main__":
     main()
